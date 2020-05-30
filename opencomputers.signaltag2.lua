787194face76b52f62e56f1710fdac214bffe3b9
Andrew B <andrew@ahbit.net>
2020-05-30 17:07:53 +1000

    Create opencomputers.signaltag2.lua

---
+++ opencomputers.signaltag2.lua
@@ -0,0 +1,62 @@
+-- Docu Referencing
+-- Requirements
+	-- Redstone Card, 2 Redstone IR Augments, 1 Controller Augment, 2 Adapters
+
+-- How it works?
+--	Set the expected direction of the loco (east/south/west/north)
+--	Set the tag to activate
+-- Redstone Refence using Redstone Card on computer NOT redstone block
+	-- Bottom (bottom), Number: 0
+	-- Top (top), Number: 1
+	-- Back (back), Number: 2
+	-- Front (front), Number: 3
+	-- Right (right), Number: 4
+	-- Left (left), Number: 5
+	--Created by Don_Spruce
+
+---- Variables Start
+local event = require("event")
+local component = require("component")
+local DetectorAugments = component.list("ir_augment_detector")
+local ControllerAugments = component.list("ir_augment_control")
+local rs = component.redstone
+--- Redstone Varaibles for intput/output
+local redsignal = 2
+local redsiglock = 3
+local redblockclear = 1
+local WantedSpeed = 2
+local Deadzone = 1
+local Threshold = 20 	-- Above this speed difference, SetChange = 1.0
+local Throttle = 0
+local ocsignaltype = 0
+local locodirection = "east"
+--- Varibles Finish
+
+
+event.listen("ir_train_overhead", function(name, address, augment_type, uuid)
+    -- Only run for ir_train_overhead with augment type of DETECTOR
+    if name ~= "ir_train_overhead" or augment_type ~= "DETECTOR" then
+        return
+    end
+  print("event triggered") --debug print
+    -- Get the detector
+    local Detector = component.proxy(address)
+    -- Sleep till train clears if it is active
+   if  string.match(stock.direction, locodirection) then
+	if (stock.tag ~= nil) then
+			-- Check to see if the stock is a loco or not
+			if string.find(Detector.info().id, "locomotives") then
+				--print("Detector.info().tag)")
+				--	print("magic1")
+					if string.find(Detector.info().tag, "_space_" ) then
+					--print("magic2")
+						rs.setOutput(redsignal,15)
+						os.sleep(0.5)
+						rs.setOutput(redsignal,0)
+					end
+				end
+			
+		end
+	end
+end)
+
